9799d2acd5b89ac44ce7dcb8ccf9b4659e86dd06
Megan Knight <mknight1@byu.edu>
2024-02-28 15:45:56 -0700

    Finished up UserDataAccess.java

--- server/src/main/java/dataAccess/UserDataAccess.java
+++ server/src/main/java/dataAccess/UserDataAccess.java
@@ -1,4 +1,47 @@
 package dataAccess;
 
-public class UserDataAccess {
+import model.UserData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class UserDataAccess implements IUserDataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.getUsername())) {
+            throw new DataAccessException("User already exists.");
+        }
+        users.put(user.getUsername(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void updateUser(UserData user) throws DataAccessException {
+        if (!users.containsKey(user.getUsername())) {
+            throw new DataAccessException("User does not exist.");
+        }
+        users.put(user.getUsername(), user);
+    }
+
+    @Override
+    public void deleteUser(String username) throws DataAccessException {
+        if (!users.containsKey(username)) {
+            throw new DataAccessException("User does not exist.");
+        }
+        users.remove(username);
+    }
+
+    @Override
+    public List<UserData> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
 }
