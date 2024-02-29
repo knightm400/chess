f39596ae303a649233dd453a4f5f2f73a2a43554
Megan Knight <mknight1@byu.edu>
2024-02-28 19:33:37 -0700

    Updated GameDataAccess.java to include methods for createGame, getGame, listGames, and updateGame

--- server/src/main/java/dataAccess/GameDataAccess.java
+++ server/src/main/java/dataAccess/GameDataAccess.java
@@ -1,7 +1,6 @@
 package dataAccess;
 
 import model.GameData;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,22 +10,23 @@ public class GameDataAccess implements IGameDataAccess {
     private Map<String, GameData> games = new HashMap<>();
 
     @Override
-    public void insertGame(GameData game) throws DataAccessException {
+    public void createGame(GameData game) throws DataAccessException {
         games.put(game.getGameName(), game);
     }
 
     @Override
-    public GameData getGame(String gameName) throws DataAccessException {
-        return games.get(gameName);
+    public GameData getGame(String gameID) throws DataAccessException {
+        return games.get(gameID);
     }
 
     @Override
-    public void deleteGame(String gameName) throws DataAccessException {
-        games.remove(gameName);
-    }
-
-    @Override
-    public List<GameData> getAllGames() throws DataAccessException {
+    public List<GameData> listGames() throws DataAccessException {
         return new ArrayList<>(games.values());
+
+    }
+
+    @Override
+    public void updateGame(String gameID, GameData game) throws DataAccessException {
+        games.put(gameID, game);
     }
 }
