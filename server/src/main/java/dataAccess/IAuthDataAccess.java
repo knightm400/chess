adb13fd06874a2cf4790aa277dac6c5d229fa879
Megan Knight <mknight1@byu.edu>
2024-02-28 20:09:49 -0700

    Completed IAuthDataAccess.java

--- server/src/main/java/dataAccess/IAuthDataAccess.java
+++ server/src/main/java/dataAccess/IAuthDataAccess.java
@@ -1,4 +1,7 @@
 package dataAccess;
 
-public class IAuthDataAccess {
+public interface IAuthDataAccess {
+    String createAuth(String username) throws DataAccessException;
+    String getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
 }
