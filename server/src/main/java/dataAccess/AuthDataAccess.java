c73dc2d7e7847caf3e7aa14e946d1b5551f04df7
Megan Knight <mknight1@byu.edu>
2024-02-28 20:13:15 -0700

    Completed AuthDataAccess.java

--- server/src/main/java/dataAccess/AuthDataAccess.java
+++ server/src/main/java/dataAccess/AuthDataAccess.java
@@ -1,4 +1,26 @@
 package dataAccess;
 
-public class AuthDataAccess {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class AuthDataAccess implements IAuthDataAccess {
+    private Map<String, String> authTokens = new HashMap<>();
+
+    @Override
+    public String createAuth(String username) throws DataAccessException {
+        String authToken = UUID.randomUUID().toString();
+        authTokens.put(authToken, username);
+        return authToken;
+    }
+
+    @Override
+    public String getAuth(String authToken) throws DataAccessException {
+        return authTokens.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authTokens.remove(authToken);
+    }
 }
