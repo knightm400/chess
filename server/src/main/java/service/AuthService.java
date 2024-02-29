0a0f8bfcf917c0087f71fd1965c2209a7d9badc7
Megan Knight <mknight1@byu.edu>
2024-02-28 20:16:01 -0700

    Completed AuthService.java

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -1,4 +1,23 @@
 package service;
 
+import dataAccess.DataAccessException;
+import dataAccess.IAuthDataAccess;
+
 public class AuthService {
+    private IAuthDataAccess authDataAccess;
+    public AuthService(IAuthDataAccess authDataAccess) {
+        this.authDataAccess = authDataAccess;
+    }
+
+    public String createAuth(String username) throws DataAccessException {
+        return authDataAccess.createAuth(username);
+    }
+
+    public String getAuth(String authToken) throws DataAccessException {
+        return authDataAccess.getAuth(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authDataAccess.deleteAuth(authToken);
+    }
 }
