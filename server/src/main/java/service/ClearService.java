761742b028f728fe8bf54b2df043c743464ad584
Megan Knight <mknight1@byu.edu>
2024-02-29 22:52:35 -0700

    Updated ClearService.java according to the instructions

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,15 +1,21 @@
 package service;
 
+import dataAccess.UserDataAccess;
+import dataAccess.GameDataAccess;
 import dataAccess.DataAccessException;
 
 public class ClearService {
-    private IUserDataAccess userDataAccess;
+    private UserDataAccess userDataAccess;
+    private GameDataAccess gameDataAccess;
 
-    public ClearService(IUserDataAccess userDataAccess) {
+    public ClearService(UserDataAccess userDataAccess, GameDataAccess gameDataAccess) {
         this.userDataAccess = userDataAccess;
+        this.gameDataAccess = gameDataAccess;
     }
 
     public void clearAll() throws DataAccessException {
         userDataAccess.clearDatabase();
+        gameDataAccess.clearDatabase();
+        // Include other data access clear methods as needed
     }
 }
