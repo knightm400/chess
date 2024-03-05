e97a20160b1cbac098d7ebcecf188592ed5587e0
Megan Knight <mknight1@byu.edu>
2024-03-04 18:55:31 -0700

    Adjusted LoginService.java

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,23 +1,30 @@
 package service;
 
+import dataAccess.AuthDataAccess;
+import dataAccess.DataAccessException;
 import dataAccess.UserDataAccess;
 import model.AuthData;
 import model.UserData;
-import dataAccess.DataAccessException;
 
 public class LoginService {
     private UserDataAccess userDataAccess;
+    private AuthDataAccess authDataAccess;
 
-    public LoginService(UserDataAccess userDataAccess) {
+    public LoginService(UserDataAccess userDataAccess, AuthDataAccess authDataAccess) {
         this.userDataAccess = userDataAccess;
+        this.authDataAccess = authDataAccess;
     }
 
     public AuthData login(String username, String password) throws DataAccessException {
         UserData user = userDataAccess.getUser(username);
-        if (user != null && user.getPassword().equals(password)) {
-            return new AuthData(username, "generated_auth_token");
-        } else {
-            throw new DataAccessException("Invalid username or password.");
+        if (user == null) {
+            throw new DataAccessException("User does not exist");
         }
+        if (!user.password().equals(password)) {
+            throw new DataAccessException("Incorrect password");
+        }
+        AuthData newAuth = new AuthData(java.util.UUID.randomUUID().toString(), username);
+        authDataAccess.insertAuth(newAuth);
+        return newAuth;
     }
 }
