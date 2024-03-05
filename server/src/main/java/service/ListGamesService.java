048bd2e99947b3613b8b7b80d9b2b778b8bcb4c1
Megan Knight <mknight1@byu.edu>
2024-03-05 00:34:50 -0700

    Completed service classes for ListGames

--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -2,17 +2,21 @@ package service;
 
 import dataAccess.GameDataAccess;
 import dataAccess.DataAccessException;
+import service.ListGamesRequest;
+import service.ListGamesResult;
 import model.GameData;
 import java.util.List;
 
 public class ListGamesService {
-    private GameDataAccess gameDataAccess;
+
+    private final GameDataAccess gameDataAccess;
 
     public ListGamesService(GameDataAccess gameDataAccess) {
         this.gameDataAccess = gameDataAccess;
     }
 
-    public List<GameData> listGames(String authToken) throws DataAccessException {
-        return gameDataAccess.getAllGames();
+    public ListGamesResult listGames(ListGamesRequest request) throws DataAccessException {
+        List<GameData> gamesList = gameDataAccess.listGames();
+        return new ListGamesResult(gamesList, true, ""); 
     }
 }
