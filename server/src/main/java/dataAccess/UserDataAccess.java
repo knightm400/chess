bfdbf24d8764215756806b8767832a79deee5943
Megan Knight <mknight1@byu.edu>
2024-03-04 18:27:35 -0700

    Updated UserDataAccess.java

--- server/src/main/java/dataAccess/UserDataAccess.java
+++ server/src/main/java/dataAccess/UserDataAccess.java
@@ -1,37 +1,12 @@
 package dataAccess;
 
 import model.UserData;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class UserDataAccess{
-    private Map<String, UserData> users = new HashMap<>();
-    public void clearDatabase() {
-        users.clear();
-    }
-
-    public void insertUser(UserData user) {
-        users.put(user.getUsername(), user);
-    }
-
-    public UserData getUser(String username) {
-        return users.get(username);
-    }
-
-    public void updateUser(UserData user) {
-        users.put(user.getUsername(), user);
-    }
-
-    public void deleteUser(String username) {
-        users.remove(username);
-    }
-
-    public List<UserData> getAllUsers() {
-        return new ArrayList<>(users.values());
-    }
-    public boolean isDatabaseEmpty() {
-        return users.isEmpty();
-    }
+public interface UserDataAccess {
+    void insertUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    void updateUser(UserData user) throws DataAccessException;
+    void deleteUser(String username) throws DataAccessException;
+    List<UserData> listUsers() throws DataAccessException;
 }
