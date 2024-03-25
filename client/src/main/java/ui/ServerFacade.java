9bd6204685a96fbf16846c853d48715965cbb774
Megan Knight <mknight1@byu.edu>
2024-03-24 21:54:41 -0600

    Created register endpoint in ServerFacade

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,48 +1,55 @@
 package ui;
 
-import dataAccess.AuthDataAccess;
-import dataAccess.GameDataAccess;
-import dataAccess.UserDataAccess;
-import dataAccess.DataAccessException;
-import model.AuthData;
-import model.GameData;
-import model.UserData;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.List;
+import com.google.gson.Gson;
+import service.Result.RegisterResult;
 
 public class ServerFacade {
-    private final UserDataAccess userDataAccess;
-    private final GameDataAccess gameDataAccess;
-    private final AuthDataAccess authDataAccess;
+    private static final String SERVER_BASE_URL = "http://localhost:8080";
+    private static final Gson gson = new Gson();
 
-    public ServerFacade(UserDataAccess userDataAccess, GameDataAccess gameDataAccess, AuthDataAccess authDataAccess) {
-        this.userDataAccess = userDataAccess;
-        this.gameDataAccess = gameDataAccess;
-        this.authDataAccess = authDataAccess;
+    public String register(String username, String password, String email) throws Exception {
+        String endpoint = "/user";
+        URL url = new URL(SERVER_BASE_URL + endpoint);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        Map<String, String> requestBodyMap = new HashMap<>();
+        requestBodyMap.put("username", username);
+        requestBodyMap.put("password", password);
+        requestBodyMap.put("email", email);
+        String requestBody = gson.toJson(requestBodyMap);
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = requestBody.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        StringBuilder response = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+        }
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 200) {
+            RegisterResult result = gson.fromJson(response.toString(), RegisterResult.class);
+            return result.authToken();
+        } else {
+            throw new Exception("Registration failed with status: " + responseCode);
+        }
     }
-
-    public UserData register(String username, String password, String email) throws DataAccessException {
-        return null;
-    }
-
-    public UserData login(String username, String password) throws DataAccessException {
-        return null;
-    }
-
-    public void logout(String authToken) throws DataAccessException {
-    }
-
-    public List<GameData> listGames(String authToken) throws DataAccessException {
-        return null;
-    }
-
-    public GameData createGame(String authToken, String gameName) throws DataAccessException {
-        return null;
-    }
-
-    public GameData joinGame(String authToken, Integer gameID) throws DataAccessException {
-        return null;
-    }
-
 }
 
